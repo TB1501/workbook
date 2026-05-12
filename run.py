c6d82c67fb828df6e1ce8dffb7d65c0aa1ad50d5
Tin Bašić <tin.basic22@gmail.com>
2026-05-12 23:50:45 +0200

    Updated index page, added login and register pages, changed run with init and routes

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template
+from app import create_app
 
-app=Flask(__name__, template_folder="app/templates")
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+app=create_app()
 
 if __name__=="__main__":
     app.run(debug=True)
