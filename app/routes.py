c7b770b647e9c43344a963e55e6210fabdd827ce
Tin Bašić <tin.basic22@gmail.com>
2026-06-08 22:08:06 +0200

    Added base template and styled the pages

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ def register_routes(app):
     @app.route("/",methods=["GET"])
     def home():
         return render_template("index.html")
+    
+    @app.route("/login", methods=["GET"])
+    def login():
+        return render_template("login.html")
+    
+    @app.route("/register", methods=["GET"])
+    def register():
+        return render_template("register.html")
